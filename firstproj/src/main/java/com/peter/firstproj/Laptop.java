ad14e9031bf8574b8c08fe4068e8a73ec0910d5b
Peter Thabet <peter.thabet@outlook.com>
2026-01-11 09:35:24 +0100

    Introduce JavaBeans and Autowiring through Spring boot

---
+++ firstproj/src/main/java/com/peter/firstproj/Laptop.java
@@ -0,0 +1,12 @@
+package com.peter.firstproj;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+        System.out.println("Compiling");
+    }
+
+}
