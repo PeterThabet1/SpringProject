7d065c48ccaea66262e69a9d1d9dab73f7c05bc1
Peter Thabet <peter.thabet@outlook.com>
2026-01-13 08:08:32 +0100

    Add changes

--- springdemo/src/main/java/com/peter/Alien.java
+++ springdemo/src/main/java/com/peter/Alien.java
@@ -1,15 +1,35 @@
 package com.peter;
 
 public class Alien {
-    int age;
-
+    
+    private int age;
+    private Laptop laptop;
 
     public Alien() {
         System.out.println("Alien Object Created ..");
+
     }
 
+    public Laptop getLaptop() {
+        return this.laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+    
     public void code(){
         System.out.println("I'm coding..");
+        laptop.compile();
+    }
+    
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Age Assigned");
+        this.age = age;
     }
 }
 
